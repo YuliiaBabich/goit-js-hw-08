e0290540f9f19814f6e8e47b0dbd5117067b963c
YuliiaBabich <aim2002@ukr.net>
2023-11-06 00:54:28 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playbackTime = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function(event) {
+    localStorage.setItem(playbackTime, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(playbackTime)) || 0);
